e576dac8ad3958e97e9f2f07ffc2790d397b4df8
rodriguezrjose95 <76861385+rodriguezjose95@users.noreply.github.com>
2021-05-12 20:07:01 -0500

    creacion de admin y estilos

---
+++ admin.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Login Administrador</title>
+	<link rel="shortcut icon" type="image/jpg" href="img/LOGO-EING.png"/>
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/style-tabla.css">
+	<link rel="stylesheet" href="librerias/bootstrap/css/bootstrap.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.css">
+	<link rel="stylesheet" href="librerias/fontawesome5/css/all.min.css">
+	<link rel="stylesheet" href="librerias/DataTables/css/jquery.dataTables.min.css">
+	<script src="librerias/bootstrap/js/bootstrap.js"></script>
+	<script src="librerias/jquery-3.4.1.min.js"></script>
+	<script src="librerias/DataTables/js/jquery.dataTables.js"></script>
+</head>
+<body class="login-admin">
+	
+	<header>
+		<a href="index.php"><img src="img/LOGO-EING.png" id="logo"></a>
+		<h1 id="titulo">ESCUELA INTERNACIONAL DE NEGOCIOS GLOBALES</h1>
+	</header>
+
+	<div class="login w-25 m-auto mt-5 bg-light">
+		
+		<h2>Login</h2>
+
+	</div>
+
+</body>
+</html>
